02e1254147d3a05e04981187494306f2c8cf10a2
Caleb Stokka and Tony Fabery <caleb+tony@>
2016-06-28 09:02:23 -0700

    returns a scrabble score for a word

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -3,10 +3,15 @@ require('pry')
 class String
   define_method(:scrabble_score) do
     letter_hash = {1 => ['a','e','i','o','u','l','n','r','s','t'], 2 =>['d','g'], 3 => ['b','c','m','p'],4 => ['f','h','v','w','y'], 5 => 'k', 8 => ['j','x'], 10 => ['q','z']}
-    letter_hash.each_key do |key|
-      if letter_hash[key].include? self
-        return key
+    word = self.split('')
+    score = 0
+    word.each_index do |idx|
+      letter_hash.each_key do |key|
+        if letter_hash[key].include? word[idx]
+          score += key
+        end
       end
     end
+    score
   end
 end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -5,5 +5,7 @@ describe('String#scrabble_score') do
   it("returns a scrabble score for a letter") do
     expect("a".scrabble_score()).to(eq(1))
   end
-  
+  it("returns a scrabble score for a word") do
+    expect("apple".scrabble_score()).to(eq(9))
+  end
 end
